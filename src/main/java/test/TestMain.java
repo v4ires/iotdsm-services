789cd8f8c1853aca2f8f75301dc037a130b47231
Vinícius Aires Barros <viniciusaires7@gmail.com>
2017-12-14 19:50:59 -0200

    setup junit framework

---
+++ src/main/java/test/TestMain.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+class TestMain {
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
