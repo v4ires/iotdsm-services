11ea7d50be4d44e10afa3c7bad3ba6b46ac65245
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-01-02 13:34:59 -0200

    update osquery scripts

--- scripts/osquery/clean-osquery-log.sh
+++ scripts/osquery/clean-osquery-log.sh
@@ -1,2 +1,1 @@
 rm -r /var/log/osquery/*
-rm -r /home/viniciusaires/iot-repository/scripts/osquery/*.result
--- scripts/osquery/run-copy-osquery.sh
+++ scripts/osquery/run-copy-osquery.sh
@@ -1,3 +1,3 @@
-cp /var/log/osquery/osqueryd.results.log /home/viniciusaires/iot-repository/scripts/osquery/osquery.d.result.log.$1.$2.$3.$4.$5.result
-chown -R viniciusaires:allusers /var/log/osquery/osqueryd.results.log /home/viniciusaires/iot-repository/scripts/osquery/osquery.d.result.log.$1.$2.$3.$4.$5.result
+cp /var/log/osquery/osqueryd.results.log /home/viniciusaires/iot-repository/scripts/osquery/osquery.d.result.log.$1.$2.result
+chown -R viniciusaires:allusers /var/log/osquery/osqueryd.results.log /home/viniciusaires/iot-repository/scripts/osquery/osquery.d.result.log.$1.$2.result
 rm -r /var/log/osquery/*
